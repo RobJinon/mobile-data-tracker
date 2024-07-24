b57cb5ee31ab1a955453e1ef64c94f4a457c2861
lim_ala_mil <lcalamil@up.edu.ph>
2024-07-24 15:41:02 +0800

    Added an input field for mobile data

--- src/components/dashboard/InputFields.jsx
+++ src/components/dashboard/InputFields.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
+function MobileDataInput(props) {
+    return (
+        <div className="join flex flex-col">
+
+            <div className="flex flex-row justify-start">
+                <label for={props.class}>{props.name}</label>
+            </div>
+            <div className="flex flex-row justify-start">
+                <input className="input input-bordered join-item" placeholder="Enter amount" />
+                <select className="select select-bordered join-item">
+                    <option>TB</option>
+                    <option selected>GB</option>
+                    <option>MB</option>
+                    <option>KB</option>
+                </select>
+            </div>
+
+        </div>
+    );
+}
+
 function DatePicker(props) {
     return (
         <div className="date-input-field flex flex-col">
-            <div className="flex flex-row justify-start"><label for="start-date">{props.name}</label></div>
+            <div className="flex flex-row justify-start"><label for={props.class}>{props.name}</label></div>
             <input type="date" className="bg-transparent p-2 border border-primary rounded" id={props.class} name={props.class}/>
         </div>
     );
@@ -18,6 +39,7 @@ function InputFields() {
                 <DatePicker name="Start Date" class="start-date" ></DatePicker>
                 <DatePicker name="Current Date" class="current-date" ></DatePicker>
                 <DatePicker name="End Date" class="end-date" ></DatePicker>
+                <MobileDataInput name="Original Data" class="original-data"></MobileDataInput>
 
             </div>
 
