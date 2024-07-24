68fe71adb8e82384b510e51c5f11a878df5e420c
lim_ala_mil <lcalamil@up.edu.ph>
2024-07-24 14:25:21 +0800

    Added custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,18 @@ export default {
     require('daisyui')
   ],
   daisyui: {
-    themes: ["light", "dark", "cupcake", "halloween", "corporate"],
+    themes: [
+      {
+        mytheme: {
+          "primary": "#14213D",
+          "neutral-0": "#E6E6E6",
+          "neutral-1": "#D9D9D9",
+          "neutral-2": "#C5C5C5",
+          "accent": "#FCA311",
+          "base-100": "#F6F6F6"
+        }
+      }
+    ],
   },
   
 }
