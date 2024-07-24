99cdbd4ccffcbc5f83cc5201661c972e9f839d18
Jon Robien Jinon <robien.jinon@gmail.com>
2024-07-24 15:01:08 +0800

    Updated UI for Signup page

--- src/signin_signup/Signup.jsx
+++ src/signin_signup/Signup.jsx
@@ -79,6 +79,9 @@ function Signup(props) {
             console.log("Enter a password.");
             errors += 1;
         }
+        if (password !== confirmPassword) {
+            console.log("Passwords don't match.");
+        }
         
         if (errors === 0){
             signupWithUsernameAndPassword(e);
@@ -87,101 +90,42 @@ function Signup(props) {
     };
 
     return (
-        <>
-            <Box
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-                minHeight="100vh"
-            >   
-                <Paper 
-                    elevation={3}
-                    sx={{
-                            p: 10,
-                            pt: 5, 
-                            width: '20%', 
-                            display: 'flex', 
-                            flexDirection: 'column',
-                            justifyContent: 'center', 
-                            alignItems: 'center'
-                        }}
-                >
+        <div className="card bg-base-100 w-3/4 lg:w-1/3 shadow-xl">
+            <div className="card-body items-center text-center">
+                <h2 className="card-title text-5xl my-5">Signup</h2>
 
-                    <Typography variant="h3" display="block" sx={{mb: 3}} gutterBottom>
-                        Sign Up
-                    </Typography>
-                    
-                    <FormControl sx={{ m: 1, width: '100%'}} variant="outlined">
-                        <InputLabel htmlFor="email">Email</InputLabel>
-                        <OutlinedInput
-                            id="outlined-adornment-password"
-                            type='text'
-                            placeholder='Email'
-                            startAdornment={
-                            <InputAdornment position="start">
-                                <EmailIcon />
-                            </InputAdornment>
-                            }
-                            label="Email"
-                            onChange = { (e) => setEmail(e.target.value) }
-                            
-                    />
-                    </FormControl>
+                <div className='flex flex-col gap-2'>
+                    <label className="input input-bordered flex items-center gap-2 mt-5">
+                        <svg xmlns="http://www.w3.org/2000/svg" className='fill-[#14213D]' height="24px" viewBox="0 -960 960 960" width="24px"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280L160-640v400h640v-400L480-440Zm0-80 320-200H160l320 200ZM160-640v-80 480-400Z"/></svg>
+                        <input type="email" className="grow" placeholder="Email" onChange={e => setEmail(e.target.value)} />
+                    </label>
 
-                    <FormControl sx={{ m: 1, width: '100%' }} variant="outlined">
-                        <InputLabel htmlFor="password">Password</InputLabel>
-                        <OutlinedInput
-                            id="password"
-                            type='password'
-                            placeholder='Password'
-                            startAdornment={
-                            <InputAdornment position="start">
-                                <PasswordIcon />
-                            </InputAdornment>
-                            }
-                            label="Password"
-                            onChange = { (e) => setPassword(e.target.value)}
-                            
-                    />
-                    </FormControl>
+                    <label className="input input-bordered flex items-center gap-2 border">
+                        <svg xmlns="http://www.w3.org/2000/svg" className='fill-[#14213D]' height="24px" viewBox="0 -960 960 960" width="24px" ><path d="M280-400q-33 0-56.5-23.5T200-480q0-33 23.5-56.5T280-560q33 0 56.5 23.5T360-480q0 33-23.5 56.5T280-400Zm0 160q-100 0-170-70T40-480q0-100 70-170t170-70q67 0 121.5 33t86.5 87h352l120 120-180 180-80-60-80 60-85-60h-47q-32 54-86.5 87T280-240Zm0-80q56 0 98.5-34t56.5-86h125l58 41 82-61 71 55 75-75-40-40H435q-14-52-56.5-86T280-640q-66 0-113 47t-47 113q0 66 47 113t113 47Z"/></svg>
+                        <input type="password" className="grow" placeholder='Password' onChange={e => setPassword(e.target.value)} />
+                    </label>
 
-                    <FormControl sx={{ m: 1, width: '100%' }} variant="outlined">
-                        <InputLabel htmlFor="confirm-password">Confirm Password</InputLabel>
-                        <OutlinedInput
-                            id="confirm-password"
-                            type='password'
-                            placeholder='Confirm Password'
-                            startAdornment={
-                            <InputAdornment position="start">
-                                <PasswordIcon />
-                            </InputAdornment>
-                            }
-                            label="Confirm Password"
-                            onChange = { (e) => setConfirmPassword(e.target.value) }
-                            
-                    />
-                    </FormControl>
+                    <label className="input input-bordered flex items-center gap-2 mb-3 border">
+                        <svg xmlns="http://www.w3.org/2000/svg" className='fill-[#14213D]' height="24px" viewBox="0 -960 960 960" width="24px" ><path d="M280-400q-33 0-56.5-23.5T200-480q0-33 23.5-56.5T280-560q33 0 56.5 23.5T360-480q0 33-23.5 56.5T280-400Zm0 160q-100 0-170-70T40-480q0-100 70-170t170-70q67 0 121.5 33t86.5 87h352l120 120-180 180-80-60-80 60-85-60h-47q-32 54-86.5 87T280-240Zm0-80q56 0 98.5-34t56.5-86h125l58 41 82-61 71 55 75-75-40-40H435q-14-52-56.5-86T280-640q-66 0-113 47t-47 113q0 66 47 113t113 47Z"/></svg>
+                        <input type="password" className="grow" placeholder='Confirm Password' onChange={e => setConfirmPassword(e.target.value)} />
+                    </label>
 
-                    <Button     
-                            type='submit' 
-                            variant='contained' 
-                            sx={{width: '100%', mt: 3}} 
-                            onClick = {(e) => validateForm(e)}>
-                                Signup
-                    </Button>
+                    <button className="btn btn-sm bg-[#14213D] rounded text-white" onClick={validateForm}>Signup</button>
 
-                    <Link to = "/login" style={{width:'100%'}}>
-                        <Button     
-                            type='submit' 
-                            variant='contained'
-                            sx={{width: '100%', mt: 1, background: 'lightgray', color: '#1f1f1f', '&:hover': {background: '#9e9e9e', color: 'white'}}}>
-                                Cancel
-                        </Button>
+                    <Link to='/' >
+                        <button className="btn btn-sm bg-[#E5E5E5] w-full rounded text-slate-700">Cancel</button>
                     </Link>
-                        
-                </Paper>
-            </Box>
-        </>
+
+                    <div className='divider'></div>
+
+                    <div className="flex flex-col lg:flex-row">
+                        <p>Already have an account?</p>
+                        <Link to='/login'><a className='link link-primary mx-1'>Sign in here.</a></Link>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     );
 }
 
