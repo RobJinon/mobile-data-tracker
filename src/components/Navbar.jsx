3bd04de1fa0ddf64f98cdf8d4e181ce90c54619e
Jon Robien Jinon <robien.jinon@gmail.com>
2024-07-25 08:47:37 +0800

    Updated register button on navbar to navigate to signup page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,9 +14,11 @@ function Navbar(props) {
                 <a className="btn btn-ghost text-xl">MOBILE DATA TRACKER</a>
             </div>
             <div class="hidden lg:block lg:flex gap-4 px-5">
-                <a className="btn btn-ghost text-slate-100">
-                    Register
-                </a>
+                <Link to='/signup'>
+                    <a className="btn btn-ghost text-slate-100">
+                        Register
+                    </a>
+                </Link>
                 <Link to='/login'>
                     <a className='btn bg-white text-slate-700'>
                         <svg xmlns="http://www.w3.org/2000/svg" className='fill-slate-700' height="24px" viewBox="0 -960 960 960" width="24px" ><path d="M480-120v-80h280v-560H480v-80h280q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H480Zm-80-160-55-58 102-102H120v-80h327L345-622l55-58 200 200-200 200Z"/></svg>
