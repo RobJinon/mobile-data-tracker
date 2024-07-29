77db5dccdf55c23047799f64d43c936c8ff8c166
lim_ala_mil <lcalamil@up.edu.ph>
2024-07-29 15:34:32 +0800

    Added input handling function for the input fields.

--- src/components/dashboard/InputFields.jsx
+++ src/components/dashboard/InputFields.jsx
@@ -8,8 +8,8 @@ function MobileDataInput(props) {
             </div>
             <div className="join flex flex-col gap-y-1 w-full h-[60%]">
                 <div className="flex flex-row justify-start h-[60%]">
-                    <input className="input input-sm bg-white border-primary rounded text-base-300 join-item w-full" placeholder="Amount" />
-                    <select className="select select-sm select-bordered bg-base-300 border-primary rounded join-item w-[55%] pl-1.5">
+                    <input type="number" name={props.class} className="input input-sm bg-white border-primary rounded join-item w-full placeholder-transparent::placeholder" placeholder="Amount"/>
+                    <select name={`${props.class}-unit`} className="select select-sm select-bordered bg-base-300 border-primary rounded join-item w-[55%] pl-1.5">
                         <option>TB</option>
                         <option selected>GB</option>
                         <option>MB</option>
@@ -28,31 +28,60 @@ function DatePicker(props) {
                 <label for={props.class}>{props.name}</label>
             </div>
             <div className="flex flex-col w-full h-[65%]">
-                <input type="date" className="bg-white border border-primary rounded py-0.5 px-1 w-full" id={props.class} name={props.class}/>
+                <input type="date" name={props.class} id={props.class} className="bg-white border border-primary rounded py-0.5 px-1 w-full placeholder-transparent::placeholder" placeholder="yolo"/>
             </div>
         </div>
     );
 }
 
 function InputFields() {
+
+    function handleSubmit(e) {
+
+        // Prevent the browser from reloading the page
+        e.preventDefault();
+
+        // Read the form data
+        const form = e.target;
+        const formData = new FormData(form);
+
+        // You can pass formData as a fetch body directly:
+        fetch('/submitData', { method: form.method, body: formData });
+
+        // Or you can work with it as a plain object:
+        const formJson = Object.fromEntries(formData.entries());
+        
+        const startDate = formJson["start-date"];
+        const currDate = formJson["current-date"];
+        const endDate = formJson["end-date"];
+
+        const origData = formJson["original-data"];
+        const origDataUnit = formJson["original-data-unit"];
+        const currData = formJson["current-data"];
+        const currDataUnit = formJson["current-data-unit"];
+
+    }
+
     return ( 
         <div className="flex flex-col gap-y-6">
-            <div id="input-fields" className="flex flex-row lg:flex-col justify-between gap-x-0.5 lg:gap-y-5">
-                <div id="date-fields" className="flex flex-col lg:flex-row justify-items-start lg:justify-between gap-y-2 w-[47%] lg:w-full">
-                    <DatePicker name="Start Date" class="start-date" ></DatePicker>
-                    <DatePicker name="Current Date" class="current-date" ></DatePicker>
-                    <DatePicker name="End Date" class="end-date" ></DatePicker>
-                </div>
-                <div id="mobile-data-fields" className="flex flex-col lg:flex-row justify-items-start lg:justify-between gap-y-2 w-[47%] lg:w-full">
-                    <MobileDataInput name="Original Data" class="original-data"></MobileDataInput>
-                    <MobileDataInput name="Original Data" class="original-data"></MobileDataInput>
-                    <div className='flex flex-col gap-y-1 justify-end lg:w-[32%]'>
-                        <button className="btn btn-sm bg-primary text-white w-full hidden lg:block lg:self-end lg:h-[55%]">COMPUTE MY DATA</button>
+            <form method="post" onSubmit={handleSubmit}>
+                <div id="input-fields" className="flex flex-row lg:flex-col justify-between gap-x-0.5 lg:gap-y-5">
+                    <div id="date-fields" className="flex flex-col lg:flex-row justify-items-start lg:justify-between gap-y-2 w-[47%] lg:w-full">
+                        <DatePicker name="Start Date" class="start-date" ></DatePicker>
+                        <DatePicker name="Current Date" class="current-date" ></DatePicker>
+                        <DatePicker name="End Date" class="end-date" ></DatePicker>
+                    </div>
+                    <div id="mobile-data-fields" className="flex flex-col lg:flex-row justify-items-start lg:justify-between gap-y-2 w-[47%] lg:w-full">
+                        <MobileDataInput name="Original Data" class="original-data"></MobileDataInput>
+                        <MobileDataInput name="Current Data" class="current-data"></MobileDataInput>
+                        <div className='flex flex-col gap-y-1 justify-end lg:w-[32%]'>
+                            <button type="submit" className="btn btn-sm bg-primary text-white w-full hidden lg:block lg:self-end lg:h-[55%]">COMPUTE MY DATA</button>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <button className="btn btn-wide bg-primary text-white w-full lg:hidden">COMPUTE MY DATA</button>
+                <button type="submit" className="btn btn-wide bg-primary text-white w-full lg:hidden">COMPUTE MY DATA</button>
+            </form>
         </div>
      );
 }
