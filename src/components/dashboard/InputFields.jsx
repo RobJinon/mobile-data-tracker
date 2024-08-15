dec036690b5bfa891e061f5674e3b825858dc66a
lim_ala_mil <lcalamil@up.edu.ph>
2024-08-15 11:53:13 +0800

    Fixed bug where newly entered inputs are retained on the input field even after switching to different ISP.

--- src/components/dashboard/InputFields.jsx
+++ src/components/dashboard/InputFields.jsx
@@ -3,24 +3,39 @@ import { auth } from '../../firebase';
 import { db } from '../../firebase';
 import { collection, query, where, getDocs, doc, updateDoc } from "firebase/firestore";
 
-function MobileDataInput(props) {
+function MobileDataInput({ id, name, numValue, setNumValue, unitValue, setUnitValue }) {
 
     // dynamically changes the default selected option of the select input field
     useEffect(() => {
-        const selectElement = document.getElementById(`${props.id}-unit`);
-        const newValue = props.unitDefaultValue;
+        const selectElement = document.getElementById(`${id}-unit`);
+        const newValue = unitValue;
         selectElement.value = newValue;
     });
     
     return (
         <div className='flex flex-col gap-y-1 lg:w-[32%]'>
             <div className="flex flex-row justify-start h-[40%]">
-                <label htmlFor={props.id}>{props.name}</label>
+                <label htmlFor={id}>{name}</label>
             </div>
             <div className="join flex flex-col gap-y-1 w-full h-[60%]">
                 <div className="flex flex-row justify-start h-[60%]">
-                    <input type="number" step="any" defaultValue={props.numDefaultValue} name={props.id} id={props.id} className="input input-sm bg-white border-primary rounded join-item w-full placeholder-transparent::placeholder" placeholder="Amount"/>
-                    <select defaultValue={props.unitDefaultValue} name={`${props.id}-unit`} className="select select-sm select-bordered bg-base-300 border-primary rounded join-item w-[55%] pl-1.5" id={`${props.id}-unit`}>
+                    <input
+                        type="number"
+                        step="any"
+                        value={numValue}
+                        onChange={(e) => setNumValue(e.target.value)}
+                        name={id}
+                        id={id}
+                        className="input input-sm bg-white border-primary rounded join-item w-full placeholder-transparent::placeholder"
+                        placeholder="Amount"
+                    />
+                    <select
+                        value={unitValue}
+                        onChange={(e) => setUnitValue(e.target.value)}
+                        name={`${id}-unit`}
+                        className="select select-sm select-bordered bg-base-300 border-primary rounded join-item w-[55%] pl-1.5"
+                        id={`${id}-unit`}
+                    >
                         <option>TB</option>
                         <option>GB</option>
                         <option>MB</option>
@@ -32,54 +47,90 @@ function MobileDataInput(props) {
     );
 }
 
-function DatePicker(props) {
+function DatePicker({ id, name, dateValue, setDateValue }) {
     return (
         <div className='flex flex-col justify-between gap-y-1.5 lg:w-[32%]'>
             <div className="flex flex-row justify-start h-[35%]">
-                <label htmlFor={props.id}>{props.name}</label>
+                <label htmlFor={id}>{name}</label>
             </div>
             <div className="flex flex-col w-full h-[65%]">
-                <input type="date" defaultValue={props.defaultValue} name={props.id} id={props.id} className="bg-white border border-primary rounded py-0.5 px-1 w-full"/>
+                <input
+                    type="date"
+                    value={dateValue}
+                    onChange={(e) => setDateValue(e.target.value)}
+                    name={id}
+                    id={id}
+                    className="bg-white border border-primary rounded py-0.5 px-1 w-full"/>
             </div>
         </div>
     );
 }
-
-const generateDatePickers = (datePickers, defaultValues) => {    
-    let items = [];
-    datePickers.map((datePicker, index) => (
-        items.push(<DatePicker name={datePicker[1]} id={datePicker[0]} defaultValue={defaultValues[index]} key={index}></DatePicker>)
-    ));
-    return items;
-};
-
-const generateNumInputs = (numInputs, numDefaultValues, unitDefaultValues) => {
-    let items = [];
-    numInputs.map((numInput, index) => (
-        items.push(<MobileDataInput name={numInput[1]} id={numInput[0]} numDefaultValue={numDefaultValues[index]} unitDefaultValue={unitDefaultValues[index]} key={index}></MobileDataInput>)
-    ));
-    return items;
-};
 
 function InputFields({ activeISP, ispList }) {
 
     const datePickers = [["start-date", "Start Date"], ["current-date", "Current Date"], ["end-date", "End Date"]];
     const numInputs = [["original-data", "Original Data"], ["current-data", "Current Data"]];
 
-    const [dateDefaultValues, setDateDefaultValues] = useState([]);
-    const [numDefaultValues, setNumDefaultValues] = useState([]);
-    const [unitDefaultValues, setUnitDefaultValues] = useState([]);
+    const [dateValues, setDateValues] = useState([]);
+    const [numValues, setNumValues] = useState([]);
+    const [unitValues, setUnitValues] = useState([]);
+
+    const generateDatePickers = (datePickers, dateValues) => {    
+        let items = [];
+        datePickers.map((datePicker, index) => (
+            items.push(
+                <DatePicker
+                    name={datePicker[1]}
+                    id={datePicker[0]}
+                    dateValue={dateValues[index]}
+                    setDateValue={(value) => {
+                        const newValues = [...dateValues];
+                        newValues[index] = value;
+                        setDateValues(newValues);
+                    }}
+                    key={index}
+                />)
+        ));
+        return items;
+    };
+
+    const generateNumInputs = (numInputs, numValues, unitDefaultValues) => {
+        let items = [];
+        numInputs.map((numInput, index) => (
+            items.push(
+                <MobileDataInput
+                    name={numInput[1]}
+                    id={numInput[0]}
+                    numValue={numValues[index]}
+                    setNumValue={(value) => {
+                        const newValues = [...numValues];
+                        newValues[index] = value;
+                        setNumValues(newValues);
+                    }}
+                    unitValue={unitValues[index]}
+                    setUnitValue={(value) => {
+                        const newValues = [...unitValues];
+                        newValues[index] = value;
+                        setUnitValues(newValues);
+                    }}
+                    key={index}
+                />)
+        ));
+        return items;
+    };
 
     // retrives ISP data from Firestore and display them as default values of the input fields
-    const displayInputs = (activeISP, ispList) => {
+    const loadInputDefaults = async(activeISP, ispList) => {
         try {
             if (activeISP) {
-                var isp = ispList.find((isp) => isp.ispName === activeISP);
+                const isp = ispList.find((isp) => isp.ispName === activeISP);
                 console.table(isp);
-                setDateDefaultValues([isp.startDate, isp.currDate, isp.endDate]);
-                setNumDefaultValues([isp.origData, isp.currData]);
-                setUnitDefaultValues([isp.origDataUnit, isp.currDataUnit]);
-            }
+                if (isp) {
+                    setDateValues([isp.startDate, isp.currDate, isp.endDate]);
+                    setNumValues([isp.origData, isp.currData]);
+                    setUnitValues([isp.origDataUnit, isp.currDataUnit]);
+                };
+            };
             console.log("Successfully displayed document data as default values of input fields.");
         } catch (error) {
             console.error("Error displaying document data as default values of input fields: " + error);
@@ -87,7 +138,7 @@ function InputFields({ activeISP, ispList }) {
     };
     
     useEffect(() => {
-        displayInputs(activeISP, ispList);
+        loadInputDefaults(activeISP, ispList);
     }, [activeISP]);
 
     // function to update the data of the user on Firestore
@@ -137,10 +188,10 @@ function InputFields({ activeISP, ispList }) {
             <form method="post" onSubmit={handleSubmit}>
                 <div id="input-fields" className="flex flex-row lg:flex-col justify-between gap-x-0.5 lg:gap-y-5">
                     <div id="date-fields" className="flex flex-col lg:flex-row justify-items-start lg:justify-between gap-y-2 w-[47%] lg:w-full">
-                        {generateDatePickers(datePickers, dateDefaultValues)}
+                        {generateDatePickers(datePickers, dateValues)}
                     </div>
                     <div id="mobile-data-fields" className="flex flex-col lg:flex-row justify-items-start lg:justify-between gap-y-2 w-[47%] lg:w-full">
-                        {generateNumInputs(numInputs, numDefaultValues, unitDefaultValues)}
+                        {generateNumInputs(numInputs, numValues, unitValues)}
                         <div className='flex flex-col gap-y-1 justify-end lg:w-[32%]'>
                             <button type="submit" className="btn btn-sm bg-primary text-white w-full hidden lg:block lg:self-end lg:h-[55%]">COMPUTE MY DATA</button>
                         </div>
