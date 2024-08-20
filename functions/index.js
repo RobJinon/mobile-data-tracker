4c212062d0f789f4228caa758947e65c202ea0da
lim_ala_mil <lcalamil@up.edu.ph>
2024-08-20 10:34:42 +0800

    Added a function to update the currDate field of every document on Firestore to be the current date every day.

---
+++ functions/index.js
@@ -0,0 +1,41 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.updateCurrentDate = functions.pubsub.schedule('every day 00:00').timeZone('Asia/Manila').onRun(async(context) => {
+    const db = admin.firestore();
+    const currDate = new Date().toISOString().split('T')[0]; // format date as YYYY-MM-DD
+    try {
+        const ispsRef = db.collection('isps');
+        const snapshot = await ispsRef.get();
+        const batch = db.batch();
+        snapshot.forEach(doc => {
+            const docRef = ispsRef.doc(doc.id);
+            batch.update(docRef, { currDate });
+        });
+        await batch.commit();
+        console.log("Successfully updated currDate for all documents");
+    } catch (error) {
+        console.error("Error updating currentDate: ", error);
+    }
+});
